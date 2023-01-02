7a03f521e30061825000b1dbfc38b528e14de02e
awgore99 <awgore99@gmail.com>
2023-01-02 09:58:46 -0600

    Added basic starting html

--- index.js
+++ index.js
@@ -71,3 +71,24 @@ function addEmployee(){
         employeeArray.push(newEmployee);
     })
 }
+
+function startHTML(){
+    const data = `<!DOCTYPE html>
+    <html lang="en"
+    <head>
+    <meta charset="UTF-8>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <title>Employee DB Manager</title>
+    </head>
+    <body>
+    <div class="container">
+        <div class="row">
+    `;
+    fs.writeFile("./dist/employeesList.html", data);
+}
+
+
+
+init();
